e199e7f06b2f61a7cdc192363fee62c4c327fa6c
Leandro <leandropereira.dev@gmail.com>
2024-03-24 10:27:12 -0300

    project completed

---
+++ program-2/script.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector('form');
+const resp1 = document.querySelector('h3');
+const resp2 = document.querySelector('h4');
+
+frm.addEventListener('submit', (e) => {
+    const title = frm.inTitulo.value
+    const duration = Number(frm.inDuracao.value)
+
+    const hours = Math.floor(duration / 60)
+    const minutes = duration % 60
+
+    resp1.innerText = title
+    resp2.innerText = `${hours} horas(s) e ${minutes} minutos(s)`
+
+    e.preventDefault()
+})
