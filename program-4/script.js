1b2ca482bab76205ac6b9c42eb9d40ad02cfb2ca
Leandro <leandropereira.dev@gmail.com>
2024-03-28 11:23:20 -0300

    changes program texts

--- program-4/script.js
+++ program-4/script.js
@@ -4,7 +4,7 @@ const resp = document.querySelector('h3');
 form.addEventListener('submit', (e) => {
     const kilo = Number(form.inKilo.value);
     const consumption = Number(form.inConsumption.value);
-
+    
     const accountValue = (kilo / 1000) * consumption;
     resp.innerText = `Valor a pagar R$: ${accountValue.toFixed(2)}`;
     e.preventDefault();
