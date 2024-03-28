66a5e0dc40b17d50bf8e419d08b8157ef6be3653
Leandro <leandropereira.dev@gmail.com>
2024-03-28 11:14:15 -0300

    JavaScript completes

---
+++ program-4/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+const resp = document.querySelector('h3');
+
+form.addEventListener('submit', (e) => {
+    const kilo = Number(form.inKilo.value);
+    const consumption = Number(form.inConsumption.value);
+
+    const accountValue = (kilo / 1000) * consumption;
+    resp.innerText = `Valor a pagar R$: ${accountValue.toFixed(2)}`;
+    e.preventDefault();
+})
